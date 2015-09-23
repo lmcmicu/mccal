560ac56d53b65e5266bfacb2affb50eb1c5cfb76
Michael Cuffaro <mike@taran>
2015-09-22 20:05:41 -0400

    Changed dialog program to zenity.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/bin/cp addappointments findappointment remind run_findapps.sh viewcal getmins ~/bin
+/bin/cp commonfuncs.pm ~/lib
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -f ~/bin/getmins ~/bin/addappointments ~/lib/Commonfuncs.pm ~/bin/findappointment ~/bin/remind ~/bin/run_findapps.sh ~/bin/viewcal
