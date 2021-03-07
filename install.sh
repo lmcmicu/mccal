0cbaf225876845e1559ca2bdf83a28ba8fe0d6f9
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-03-07 15:25:48 -0500

    add pause and unpause scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/bin/cp addappointments findappointment remind run_findapps.sh viewcal getmins synccal ~/bin
+/bin/cp pause_mccal unpause_mccal addappointments findappointment remind run_findapps.sh viewcal getmins synccal ~/bin
 /bin/cp commonfuncs.pm ~/lib
